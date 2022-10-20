83b196233ff76828b4601d2b8196946e218bd516
Eliseev Denis <medved-ed@mail.ru>
2022-10-21 02:14:09 +0800

    seminar 3 zad3 zad4

--- seminar3_zad3.py
+++ seminar3_zad3.py
@@ -2,3 +2,29 @@
 # Напишите программу, которая найдёт разницу между 
 # максимальным и минимальным значением дробной части элементов.
 
+def randlistfloat(x):
+    from random import randint
+    array = []
+    for i in range (x):
+        array.append (randint(100, 1000)/100)
+    return array
+
+def minmaxdiff (array):
+    imax = 0
+    imin = 0
+    for i in range (len (array)):
+        if  array [i]%1  > array [imax]%1:
+            imax = i
+        elif array [i]%1  < array [imin]%1:
+            imin = i
+    return round ((array [imax]%1 - array [imin]%1),2)
+        
+
+
+try:
+    n = int (input ('Введите необходимое количество чисел в списке: '))
+    list_var = randlistfloat (n)
+    print (list_var)
+    print (minmaxdiff (list_var))
+except:
+    print ('Ошибка!! Введите целое число!')
